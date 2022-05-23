3bab10c9133b67a9fbed03146014d5381d9ff61e
Jyoti Ranjan Kalta <70003870+jyotiranjankalta81@users.noreply.github.com>
2022-05-23 12:02:07 +0530

    Update Footer.js
    some update

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2020 &copy; Jyoti Ranjan Kalta</p>
       </div>
 
       <div className="rightFooter">
